4124d3ef8aeb555d0615b3e3fd645aaa89ef205c
Luis Zavala <94495979+Luizzavala@users.noreply.github.com>
2025-06-06 19:31:41 -0700

    refactor: move sidebar component to shared

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from './features/dashboard/sidebar/sidebar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
